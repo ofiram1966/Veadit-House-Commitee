a6730f19799b9f48decdd9b6415532c0404bbc42
ofiram <ofiram7@gmail.com>
2018-12-05 13:26:01 +0200

    adding angular routinig + home html

--- app/app.js
+++ app/app.js
@@ -1,1 +1,37 @@
-var app = angular.module("veaditApp", []);
+var app = angular.module("HouseCommitteeApp", ["ngRoute", ]);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "app/home/home.html",
+            // controller: "homeCtrl"
+        })
+        .when("/login", {
+            templateUrl: "app/login/login.html",
+            controller: "loginCtrl"
+        })
+        .when("/dashboard", {
+            templateUrl: "app/dashboard/dashboard.html",
+            controller: "dashboardCtrl"
+        })
+        .when("/messages", {
+            templateUrl: "app/messages/messages.html",
+            controller: "messageCtrl"
+        })
+        .when("/issues", {
+            templateUrl: "app/issues/issues.html",
+            controller: "issueCtrl"
+        })
+        .when("/votings", {
+            templateUrl: "app/votings/votings.html",
+            controller: "votingCtrl"
+        })
+        .when("/tenants", {
+            templateUrl: "app/tenants/tenants.html",
+            controller: "tenantsCtrl"
+        })
+        .when("/signUp", {
+            templateUrl: "app/signUp/signUp.html",
+            controller: "signUpCtrl"
+        })
+});
