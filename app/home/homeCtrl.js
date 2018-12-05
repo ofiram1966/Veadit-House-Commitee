25e5f4bef22f7faccf983dbb676585bfb1a426fe
ofiram <ofiram7@gmail.com>
2018-12-05 23:23:17 +0200

    add popover discribtion for home html

--- app/home/homeCtrl.js
+++ app/home/homeCtrl.js
@@ -1,6 +1,14 @@
 
-app.controller("homeCtrl", function($scope, $uibModal) {
+app.controller("homeCtrl", function($scope) {
 
-  
+    $scope.showPopover = function() {
+        $scope.popoverIsVisible = true; 
+        delay: {show:500; hide:100}
+      };
+      
+      $scope.hidePopover = function () {
+        $scope.popoverIsVisible = false;
+        
+      };
   
 });
