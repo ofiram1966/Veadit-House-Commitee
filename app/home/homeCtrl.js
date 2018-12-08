87428a2b63933057492bce26d3859e03bfc92aec
ofiram <ofiram7@gmail.com>
2018-12-08 22:14:18 +0200

    update db json+home html

--- app/home/homeCtrl.js
+++ app/home/homeCtrl.js
@@ -1,22 +1,36 @@
 
 app.controller("homeCtrl", function($scope) {
 
-    $scope.showPopover = function(section) {
+    $scope.showPopover = function() {
         $scope.popoverIsVisible = true; 
         
       };
-      
       $scope.hidePopover = function () {
         $scope.popoverIsVisible = false;
-        
       };
+      
       $scope.showPopover1 = function() {
         $scope.popoverIsVisible1 = true; 
-        
       };
       
       $scope.hidePopover1 = function () {
         $scope.popoverIsVisible1 = false;
+      };
+     
+      $scope.showPopover2 = function() {
+        $scope.popoverIsVisible2 = true; 
+      };
+      
+      $scope.hidePopover2 = function () {
+        $scope.popoverIsVisible2 = false;
+        
+      };
+      $scope.showPopover3 = function() {
+        $scope.popoverIsVisible3 = true; 
+      };
+      
+      $scope.hidePopover3 = function () {
+        $scope.popoverIsVisible3 = false;
         
       };
       
--- app/navbar/navbarCtrl.js
+++ app/navbar/navbarCtrl.js
@@ -2,7 +2,16 @@ app.controller("navbarCtrl",function($scope ){
 
   $scope.isUserLoggedIn=function(){
       return true;
-  
   }
+//   $scope.isUserLoggedIn = function () {
+//     return user.isLoggedIn();
+// }
+//  $scope.isUserAdmin = function () {
+//     return user.isAdmin();
+// }
+//  $scope.logout = function () {
+//     user.logout();
+//     $location.path("/");
+// }
 
 })
