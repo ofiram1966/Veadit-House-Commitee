940b09d308b5b1999565d948c5ca7b4cda955404
ofiram <ofiram7@gmail.com>
2018-12-14 15:05:01 +0200

    update dbjason

--- app/messages/messagesCtrl.js
+++ app/messages/messagesCtrl.js
@@ -1,65 +1,13 @@
-app.controller("messagesCtrl",function($scope, user, $location ){
-    $scope.activeUser = user.getActiveUser();
-    $scope.messages=[
-    {
+app.controller("messagesCtrl",function($scope,user, messages ){
+    
+   
+messages.getActiveUserMessages().then(function (messages){
+    $scope.messages = messages;
 
-        id: 1,
-        memberId: 1,
-        communityId:1,
-        creationTime: "2018-11-03T12:34:44.110Z",
-        title: "1st message",
-        details: "Message details...blabla..blabla...",
-        priority: "High",
-        imgUrl:"assets/images/message7.jpg"
-    },
-    {
+},function(error){
 
-        id: 2,
-        memberId: 1,
-        communityId:1,
-        creationTime: "2018-11-03T12:34:44.110Z",
-        title: "2nd message",
-        details: "Message details...blabla..blabla...",
-        priority: "High",
-        imgUrl:"assets/images/message7.jpg"
-    },
+})
 
-    {
-
-        id: 3,
-        memberId: 1,
-        communityId:1,
-        creationTime: "2018-11-03T12:34:44.110Z",
-        title: "3rd message",
-        details: "Message details...blabla..blabla...",
-        priority: "High",
-        imgUrl:"assets/images/message7.jpg"
-    },
-
-    {
-
-        id: 4,
-        memberId: 1,
-        communityId:1,
-        creationTime: "2018-11-03T12:34:44.110Z",
-        title: "4th message",
-        details: "Message details...blabla..blabla...",
-        priority: "High",
-        imgUrl:"assets/images/message7.jpg"
-    }
-
-
-    ]
-
-    // if (!user.isLoggedIn()) {
-    //     $location.path("/");
-    //     return;
-    // }
-    // message.getActiveUserMessages().then(function (messages) {
-    //     $scope.messages = messages;
-    // }, function(error) {
-        
-    // })
 
 
 });
---
+++ app/model/messagesSrv.js
@@ -0,0 +1,48 @@
+app.factory("messages", function($q, $http,user) {
+
+     var messages = [];
+    
+    function Message(plainMessage) {
+        this.id = plainMessage.id;
+        this.userId = plainMessage.userId;
+        this.communityId = plainMessage.communityId;
+        this.creationTime = plainMessage.creationTime;
+        this.title = plainMessage.title;
+        this.details = plainMessage.details;
+        this.priority = plainMessage.priority;
+        this.comments = plainMessage.comments;
+    }
+
+    function getActiveUserMessages(){
+        
+        var async = $q.defer();
+    
+
+        var getMessagesURL =  "https://my-json-server.typicode.com/ofiram1966/Veadit-House-Commitee/messages?userId=" + 
+        user.getActiveUser().id;
+
+        $http.get(getMessagesURL).then(function (response) {
+            for (var i=0; i<response.data.length; i++){
+                var message = new Message(response.data[i]);
+                messages.push(message);
+            }
+            async.resolve(messages);
+
+        }, function (error) {
+            async.reject(error);
+                   
+        })
+
+         return async.promise;
+    }
+    return {
+        getActiveuserMessages : getActiveuserMessages
+        
+    }
+})
+
+
+   
+
+  
+
